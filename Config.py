8558df5704dc7f65dea39bb5b6175103d89b6e6d
GMercat <guillaume.mercat@gmail.com>
2015-02-28 21:43:25 +0100

    Ajout du ficheir de configuration

---
+++ Config.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# -*- coding: utf8 -*-
+
+FichierEnregistrementOn = '/mon/repertoire/serveur/OnRecord'
+RepertoirePhotosTmp     = '/mon/repertoire/photos/Tmp/'
+RepertoirePhotos        = '/mon/repertoire/photos/'
+RepertoireServer        = '/mon/repertoire/serveur/'
+LastPhoto               = 'Last.jpg'
+PhotoT1                 = 'Photo1.jpg'
+PhotoT2                 = 'Photo2.jpg'
+PhotoTDiff              = 'PhotoDiff.jpg'
+PhotoHeight             = 480
+PhotoWidth              = 640
+SeuilMouvement          = 10
+MailOrig                = 'adresse.pi@mail.com'
+MailDest                = 'mon.adresse@mail.com'
+MailPassword            = 'mondepassesupersecret'
+MailSMTP                = 'serveur.smtp:port'
